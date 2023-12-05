6f3f15baf72cc81aa256cda7af486d84c87283e6
Jelles <jelles.duin@gmail.com>
2023-12-05 21:08:18 +0100

    removed method map

--- assets/js/types/Boid.ts
+++ assets/js/types/Boid.ts
@@ -64,10 +64,6 @@ export class Boid {
         return steer;
     }
 
-    map(value: number, start1: number, stop1: number, start2: number, stop2: number) {
-        return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-    }
-
     wander() {
         let wanderR = 25;
         let wanderD = 80;
